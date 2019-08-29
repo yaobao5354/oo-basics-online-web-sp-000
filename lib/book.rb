1fd76473e21d477a0eeb68e1ca9f17c2afbb7a7a
yaoyu bao <yaobao5354@gmail.com>
2019-08-29 06:51:14 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,39 +1,17 @@
+ 
 class Book
+  attr_accessor :author, :page_count, :genre
+ 
   def initialize(title)
-    @title = title 
-  end 
-  
+    @title = title
+  end
+ 
   def title
-    @title 
-  end 
-  
-  def author=(author)
-    @author = author
-  end 
-  
-  def author
-    @author 
-  end 
-  
-  def page_count=(num)
-    @page_count = num
-  end 
-  
-  def page_count
-    @page_count
-  end 
-  
-  def genre=(genre)
-    @genre = genre
-  end 
-  
-  def genre 
-    @genre
-  end 
-  
+    @title
+  end
+ 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
-  end 
-  
-end 
-
+  end
+ 
+end
