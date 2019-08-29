58c13fc7d90d83c9b2c5efe1d161b00fe3148e9b
yaoyu bao <yaobao5354@gmail.com>
2019-08-29 06:54:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,12 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :color :
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand 
+  end 
+  
+  def brand=(brand)
+    @brand 
 end 
